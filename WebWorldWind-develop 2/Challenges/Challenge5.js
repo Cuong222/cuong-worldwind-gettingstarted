2d9950a65d147adefa3eb71b334c0dd2148583cc
imacbig06 <charlie.cai@g.feitianacademy.org>
2021-04-30 15:08:57 -0400

    Getting Started

---
+++ WebWorldWind-develop 2/Challenges/Challenge5.js
@@ -0,0 +1,314 @@
+
+
+
+requirejs(['../examples/WorldWindShim',
+        '../examples/LayerManager'],
+    function (WorldWind, LayerManager) {
+        "use strict";
+
+        // Tell WorldWind to log only warnings and errors.
+        WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
+
+        // Create the WorldWindow.
+        var wwd = new WorldWind.WorldWindow("canvasOne");
+
+        // Create and add layers to the WorldWindow.
+        var layers = [
+            // Imagery layers.
+            {layer: new WorldWind.BMNGLayer(), enabled: true},
+            {layer: new WorldWind.BMNGLandsatLayer(), enabled: false},
+            {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: true},
+            // Add atmosphere layer on top of all base layers.
+            {layer: new WorldWind.AtmosphereLayer(), enabled: true},
+            // WorldWindow UI layers.
+            {layer: new WorldWind.CompassLayer(), enabled: true},
+            {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
+            {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
+        ];
+
+        for (var l = 0; l < layers.length; l++) {
+            layers[l].layer.enabled = layers[l].enabled;
+            wwd.addLayer(layers[l].layer);
+        }
+
+        // Web Map Service information from NASA's Near Earth Observations WMS
+        var serviceAddress = "http://aworldbridgelabs.com:8080/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities";
+        // // // Named layer displaying Average Temperature data
+        var layerName = "Cuong_Challenge5:Challenge5";
+        //
+        var createLayer = function (xmlDom) {
+
+            // Create a WmsCapabilities object from the XML DOM
+            var wms = new WorldWind.WmsCapabilities(xmlDom);
+            // Retrieve a WmsLayerCapabilities object by the desired layer name
+            var wmsLayerCapabilities = wms.getNamedLayer(layerName);
+            // Form a configuration object from the WmsLayerCapability object
+            var wmsConfig = WorldWind.WmsLayer.formLayerConfiguration(wmsLayerCapabilities);
+            // Modify the configuration objects title property to a more user friendly title
+            wmsConfig.title = "WMS";
+            // Create the WMS Layer from the configuration object
+            var wmsLayer = new WorldWind.WmsLayer(wmsConfig);
+
+            // Add the layers to WorldWind and update the layer manager
+            wwd.addLayer(wmsLayer);
+            layerManager.synchronizeLayerList();
+            console.log(wwd.layers);
+
+            document.getElementById("check2").addEventListener("change", function(){
+                if (this.checked){
+                    wmsLayer.enabled = true;
+                    console.log(wmsLayer.enabled);
+                } else {
+                    wmsLayer.enabled = false;
+                    console.log(wmsLayer.enabled)
+                }
+            });
+
+        };
+
+        // // Called if an error occurs during WMS Capabilities document retrieval
+        var logError = function (jqXhr, text, exception) {
+            console.log("There was a failure retrieving the capabilities document: " + text + " exception: " + exception);
+        };
+        //
+        $.get(serviceAddress).done(createLayer).fail(logError);
+        // Define the images we'll use for the placemarks.
+        var images = [
+            "plain-black.png",
+            "plain-blue.png",
+            "plain-white.png"
+        ];
+
+        var pinLibrary = WorldWind.configuration.baseUrl + "images/pushpins/", // location of the image files
+            placemark,
+            placemarkAttributes = new WorldWind.PlacemarkAttributes(null),
+            highlightAttributes,
+            placemarkLayer = new WorldWind.RenderableLayer("Placemarks");
+        var latitude = [];
+        var longitude = [];
+        latitude = [21.0278, 32.8032, 41.4459];
+        longitude = [105.8342, 130.7079, -74.4229];
+
+        //toggle to hide and open placemark
+        document.getElementById("check1").addEventListener("change", function(){
+            if (this.checked){
+                placemarkLayer.enabled = true;
+            } else {
+                placemarkLayer.enabled = false;
+            }
+        });
+
+        // document.getElementById("check2").addEventListener("change", function(){
+        //     if (this.checked){
+        //         wmsLayer.enabled = false;
+        //         console.log(wmsLayer.enabled);
+        //     } else {
+        //         wmsLayer.enabled = true;
+        //         console.log(wmsLayer.enabled)
+        //     }
+        // });
+
+        // Set up the common placemark attributes.
+        placemarkAttributes.imageScale = 5;
+        placemarkAttributes.imageOffset = new WorldWind.Offset(
+            WorldWind.OFFSET_FRACTION, 0.3,
+            WorldWind.OFFSET_FRACTION, 0.0);
+        placemarkAttributes.imageColor = WorldWind.Color.WHITE;
+        placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
+            WorldWind.OFFSET_FRACTION, 0.5,
+            WorldWind.OFFSET_FRACTION, 1.0);
+        placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
+        placemarkAttributes.drawLeaderLine = true;
+        placemarkAttributes.leaderLineAttributes.outlineColor = WorldWind.Color.RED;
+
+        // For each placemark image, create a placemark with a label.
+        for (var i = 0, len = images.length; i < len; i++) {
+            // Create the placemark and its label.
+            placemark = new WorldWind.Placemark(new WorldWind.Position(latitude[i], longitude[i], 1e2), true, null);
+            console.log(placemark);
+            placemark.label = "Placemark " + i.toString() + "\n"
+                + "Lat " + placemark.position.latitude.toPrecision(4).toString() + "\n"
+                + "Lon " + placemark.position.longitude.toPrecision(5).toString();
+            placemark.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
+
+            // Create the placemark attributes for this placemark. Note that the attributes differ only by their
+            // image URL.
+            placemarkAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
+            placemarkAttributes.imageSource = pinLibrary + images[i];
+            placemark.attributes = placemarkAttributes;
+
+            // Create the highlight attributes for this placemark. Note that the normal attributes are specified as
+            // the default highlight attributes so that all properties are identical except the image scale. You could
+            // instead vary the color, image, or other property to control the highlight representation.
+            highlightAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
+            highlightAttributes.imageScale = 6;
+            placemark.highlightAttributes = highlightAttributes;
+
+            // Add the placemark to the layer.
+            placemarkLayer.addRenderable(placemark);
+        }
+
+        // Add the placemarks layer to the WorldWindow's layer list.
+        wwd.addLayer(placemarkLayer);
+
+        // Now set up to handle picking.
+
+        var highlightedItems = [];
+
+        // The common pick-handling function.
+        var handlePick = function (o) {
+            // The input argument is either an Event or a TapRecognizer. Both have the same properties for determining
+            // the mouse or tap location.
+            var x = o.clientX,
+                y = o.clientY;
+
+            var redrawRequired = highlightedItems.length > 0; // must redraw if we de-highlight previously picked items
+
+            // De-highlight any previously highlighted placemarks.
+            for (var h = 0; h < highlightedItems.length; h++) {
+                highlightedItems[h].highlighted = false;
+            }
+            highlightedItems = [];
+
+            // Perform the pick. Must first convert from window coordinates to canvas coordinates, which are
+            // relative to the upper left corner of the canvas rather than the upper left corner of the page.
+            var pickList = wwd.pick(wwd.canvasCoordinates(x, y));
+            console.log(pickList)
+            if (pickList.objects.length > 0) {
+                redrawRequired = true;
+            }
+
+            // Highlight the items picked by simply setting their highlight flag to true.
+            if (pickList.objects.length > 0) {
+                for (var p = 0; p < pickList.objects.length; p++) {
+                    pickList.objects[p].userObject.highlighted = true;
+
+                    // Keep track of highlighted items in order to de-highlight them later.
+                    highlightedItems.push(pickList.objects[p].userObject);
+
+                    // Detect whether the placemark's label was picked. If so, the "labelPicked" property is true.
+                    // If instead the user picked the placemark's image, the "labelPicked" property is false.
+                    // Applications might use this information to determine whether the user wants to edit the label
+                    // or is merely picking the placemark as a whole.
+                    if (pickList.objects[p].labelPicked) {
+                        console.log("Label picked");
+                    }
+
+                    //1st placemark
+                    if (pickList.objects[p].position.longitude === longitude[0]) {
+                        document.getElementById("mmodal").style.visibility = "visible";
+                    };
+
+                    //close 1st placemark
+                    var close = document.getElementsByClassName("close");
+                    var closem = function () {
+                        document.getElementById("mmodal").style.visibility = "hidden";
+                    }
+                    close[0].addEventListener("click", closem);
+                    console.log(close);
+
+
+                    //2nd placemark
+                    if (pickList.objects[p].position.longitude === longitude[1]) {
+                        document.getElementById("kmodal").style.visibility = "visible";
+                    };
+                    //close placemark 2
+                    var close = document.getElementsByClassName("close");
+                    var closek = function () {
+                        document.getElementById("kmodal").style.visibility = "hidden";
+                    }
+                    close[1].addEventListener("click", closek);
+
+
+                    // //3rd placemark
+                    //
+                    // if (pickList.objects[p].position.longitude === longitude[2]) {
+                    //     document.getElementById("lmodal").style.visibility = "visible";
+                    // };
+                    //
+                    // //close placemark 3
+                    // var close = document.getElementsByClassName("close");
+                    // var closel = function () {
+                    //     document.getElementById("lmodal").style.visibility = "hidden";
+                    // }
+                    // close[2].addEventListener("click", closel);
+                }
+            }
+            // put one placemark on Kumamoto and Middletown NY
+
+            // Update the window if we changed anything.
+            if (redrawRequired) {
+                wwd.redraw(); // redraw to make the highlighting changes take effect on the screen
+            }
+        };
+
+        // Listen for mouse moves and highlight the placemarks that the cursor rolls over.
+        wwd.addEventListener("click", handlePick);
+
+        var handleMove = function (o) {
+            // The input argument is either an Event or a TapRecognizer. Both have the same properties for determining
+            // the mouse or tap location.
+            var x = o.clientX,
+                y = o.clientY;
+
+            var redrawRequired = highlightedItems.length > 0; // must redraw if we de-highlight previously picked items
+
+            // De-highlight any previously highlighted placemarks.
+            for (var h = 0; h < highlightedItems.length; h++) {
+                highlightedItems[h].highlighted = false;
+            }
+            highlightedItems = [];
+
+            // Perform the pick. Must first convert from window coordinates to canvas coordinates, which are
+            // relative to the upper left corner of the canvas rather than the upper left corner of the page.
+            var pickLists = wwd.pick(wwd.canvasCoordinates(x, y));
+            console.log(pickLists)
+            if (pickLists.objects.length > 0) {
+                redrawRequired = true;
+            }
+
+            // Highlight the items picked by simply setting their highlight flag to true.
+            if (pickLists.objects.length > 0) {
+                for (var p = 0; p < pickLists.objects.length; p++) {
+                    pickLists.objects[p].userObject.highlighted = true;
+
+                    // Keep track of highlighted items in order to de-highlight them later.
+                    highlightedItems.push(pickLists.objects[p].userObject);
+
+                    // Detect whether the placemark's label was picked. If so, the "labelPicked" property is true.
+                    // If instead the user picked the placemark's image, the "labelPicked" property is false.
+                    // Applications might use this information to determine whether the user wants to edit the label
+                    // or is merely picking the placemark as a whole.
+                    if (pickLists.objects[p].labelPicked) {
+                        console.log("Label picked");
+                    }
+
+                    //3rd placemark
+
+                    if (pickLists.objects[p].position.longitude === longitude[2]) {
+                        document.getElementById("lmodal").style.visibility = "visible";
+                    };
+
+                    //close placemark 3
+                    var close = document.getElementsByClassName("close");
+                    var closel = function () {
+                        document.getElementById("lmodal").style.visibility = "hidden";
+                    }
+                    close[2].addEventListener("click", closel);
+                }
+            }
+            // put one placemark on Kumamoto and Middletown NY
+
+            // Update the window if we changed anything.
+            if (redrawRequired) {
+                wwd.redraw(); // redraw to make the highlighting changes take effect on the screen
+            }
+        };
+        wwd.addEventListener("mousemove", handleMove);
+
+        // Listen for taps on mobile devices and highlight the placemarks that the user taps.
+        var tapRecognizer = new WorldWind.TapRecognizer(wwd, handlePick);
+
+        // Create a layer manager for controlling layer visibility.
+        var layerManager = new LayerManager(wwd);
+    });
